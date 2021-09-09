cd5a7aa2e163c3dd7a46d81dabe5252819c0775b
Pan Zhan <panzhan@Pans-MacBook-Pro.local>
2021-09-09 21:56:50 +1000

    simple server response

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello world</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(5000);
